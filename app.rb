b9a4d043ec4249897e9dbb596144fabf025d9e01
Eung Jin Lee <oiojin831@gmail.com>
2016-11-21 21:24:18 +0900

    update

--- app.rb
+++ app.rb
@@ -14,13 +14,21 @@ post '/ddabong' do
   if text[0] == "give"
     # save data to db
     # check db with name
-    puts params[:user_name]
-    puts text[1]
+    p params[:user_name]
+    p text[1]
     from = User.find_by(name: params[:user_name])
     to = User.find_by(name: text[1])
 
     Ddabong.create(from: from.id, to: to.id)
     return { :text => "#{text[1]}님은 #{params[:user_name]}님으로부터 따봉 하나를 받았습니다.", response_type: "in_channel", }.to_json
+  elsif text[0] == "create"
+    if text[1] == "me"
+      u = User.find_or_create_by(name: params[:user_name])
+      return { :text => "유저 아이디 #{u.id}, #{params[:user_name]}" }
+    else
+      u = User.find_or_create_by(name: text[1])
+      return { :text => "유저 아이디 #{u.id},#{text[1]}" }
+    end
   elsif text[0] == "check"
     if user = User.find_by(name: text[1])
       puts user
