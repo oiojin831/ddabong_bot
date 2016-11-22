c24d4e2d4ad6357c3a60df2959cc0043020ee0df
Eung Jin Lee <oiojin831@gmail.com>
2016-11-22 13:12:00 +0900

    fix to slash

--- app.rb
+++ app.rb
@@ -11,11 +11,8 @@ end
 post '/ddabong' do
   content_type :json
   text = params[:text].split(" ")
+
   if text[0] == "give"
-    # save data to db
-    # check db with name
-    p params[:user_name]
-    p text[1]
     from = User.find_or_create_by(name: params[:user_name])
     to = User.find_or_create_by(name: text[1])
 
@@ -23,12 +20,9 @@ post '/ddabong' do
     return { :text => "#{text[1]}님은 #{params[:user_name]}님으로부터 따봉 하나를 받았습니다.", response_type: "in_channel", }.to_json
   elsif text[0] == "check"
     user = User.find_or_create_by(name: text[1])
-    p user
     dda = Ddabong.where(to: user.id)
     count = dda.count
-    p dda
-    p count.to_s
-    return { :text => "총 따봉 #{count.to_s}" }.to_json
+    return { :text => "총 따봉 #{count.to_s}" , response_type: "in_channel", }.to_json
   end
 end
 
